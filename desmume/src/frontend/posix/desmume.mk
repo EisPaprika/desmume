fd69e32a0bec7bfbb7cbede9f57d57320058d252
EisPaprika <floriankiera135@gmail.com>
2025-07-24 21:12:43 +0200

    Fixed include for gtk lua

--- desmume/src/frontend/posix/desmume.mk
+++ desmume/src/frontend/posix/desmume.mk
@@ -1,6 +1,5 @@
 AM_CFLAGS = 
-AM_CPPFLAGS = -I$(top_srcdir)/../../../src/ -I$(top_srcdir)/../../../src/libretro-common/include -I$(top_srcdir)/../../../src/frontend/windows/lua/include
-
+AM_CPPFLAGS = -I$(top_srcdir)/../../../src/ -I$(top_srcdir)/../../../src/libretro-common/include
 #add this so that frontends can use "../types.h" for instance (they were built expecting to be in subdirectories of the main desmume dir
 AM_CPPFLAGS += -I$(top_srcdir)/../../../src/frontend
 
